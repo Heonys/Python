2fb2f7aaf3b8706723ea68c2383394cd960af1d0
heon <siwmua0@naver.com>
2022-01-13 00:30:58 +0900

    정리

--- 청해진/test.py
+++ 청해진/test.py
@@ -1,51 +1,6 @@
-# 0. **kwargs >> 함수의 파라미터로 **을 주면 딕셔너리가 된다 
-# 0. return >> return으로 함수를 즉시 빠져나가는 방법을 자주 사용한다, 
-# 0. 항상변하는게 아닐때 함수의 매개변수 초기값 선언을 해준다 (매개변수=값)
-#    단 초기화시키고 싶은 매개변수를 항상 뒤쪽 
-# 0. input() + split() 으로 입력받은거 나눔 map()이나 강제형변환 응용해서씀
-# 0. global >> 함수는 독립적으로 존재하는 것이 좋기 때문에 가급적 x 
-# 
-# 0. lamda >> 익명함수 ,함수가 복잡하지 않을때 함수를 한줄로 간결하게 만들때 사용 
-#     ex) 메모리절약, 함수안에서 def를 사용하지 못할때 >> (lambda a, b : a+b)(3,4)
-# 0.str.strip() >> 문자열 공백 제거 
-# 0. sum(a) >> 리스트나 튜플의 합을 구함 sum(a,b) 이런식으로 쓰는 거 아님 
-# 0. list.sort()  >> 리스트를 정렬해서 변환
-#    sorted(list) >> 본체는 냅두고 정렬할 새로운 리스트 반환
-# 0. 특이한 파이썬의 and, or 연산값 
-# 0. ==는 값을 비고 is는 객체를 비교 (고유의 id값으로)
-# 0. x in list, x not in list 값이 있는지 없는지 확인 (리스트, 문자열, 튜플, 딕셔너리, 집합 등)
-# 0. 반복문에 else 사용가능 >> while의 else는 if때와 동일, for문은 모두 실행된 후 else 실행 
-# 0. 재귀함수를 통해 점화식을 표현가능 
-# 0. 딕셔너리는 요솟값을 구하려고할때 단한가지임 > KEY값을 이용한 방법 // 애초에 순서가 없어서 인덱스 사용 안됨
-# 0. dict.get(key,n) = key값에 해당하는 값이 없으면 n을 리턴해서 오류가 발생하지 않음 >> n 생략하면 기본값은 none
-# 0. pop() vs dell 차이 둘다 인덱스로 값을 지우지만 pop은 지운것을 반환 dell은 반환안함
-#    remove() >> pop, dell과 다르게 값으로 지움, 중복시 먼저나온 값 // 3개다 원본 값을 바꾸기때문에 주의
-# 0. 음수 index >> 맨 뒤부터 -1임 
-# 0. 변수에 객체를 할당하면 네임스페이스 안에 저장 // 상위(밖)는 하위(안) 참조x  , 하위는 상위 참조가능 
-# 0. packing >> 함수의 인자의 개수를 유연하게 해줌 def func(*args): print(args)  >> 매개변수 앞에 *
-# 0. unpacking은 여러개의 객체를 포함하고 있는 하나의 객체를 풀어줌 >> 인자 앞에 * 
-# 0. 에러발생시키기 > raise
-# 0. 예외처리 >> try >>  except
-# 0. 컴프리헨션 >> [x for x in range(10)] 와 같이 간단히 줄여쓰도록 지원하는 것 
-# 0. 컴프리헨션 문법을 사용하면 제너레이터가 생성됨  >> 리스트는 평가되있기 때문에 제너레이터 생성x 
-# 0. 제너레이터는 기억하지않는 1회용 이라서 소비되고 next(gen) 함수로 하나씩 사용가능 
 
-# 비교적 자주쓰는 내장함수!~
-# 0. abs() > 절대값 // all() > iterable자료형의 각항들이 전부 참일때 만 참 // any() > 하나라도 참이면 참 all()과 반대
-#    divmod(a, b) > a/b, a%b를 튜플로 반환 // filter() > map()과 문법 동일 조건에 의해 iterable자료형을 걸러내는 함수 (for문과 같이씀)
-#    hex() > 16진수 // oct() > 8진수 // pow(x, y) > x의 y제곱
-#
-# 0. list(range(5, 10)) [5, 6, 7, 8, 9]
-# 0. enumerate() // 주로 for문과 같이사용하여 iterable자료형을 (인덱스 , 값 ) 의 튜플로 반환 // 당연히 str도 가능함
-# 0. pickle 모듈 사용하기 // pickle.dump(data, file) 넣기 pickle.load(file)로 불러오기
-# 0. time.sleep(1) // 잠시 재우기  time.ctime() // 현재시간
-# 0. random.randint(1, 10) // 1 ~ 10 무작위 난수 // choice > 무작위 가져옴 // shuffle 섞기
-# 0. join() >>  '구분자'.join(리스트)
-# 0. 예외처리시 에러 발생하는 순간 탈출 후 finally
-# 0. list >> string 변환시  ''.join(list) // ** 단 list의 모든 원소가 str형태여야함 정수,실수이면 join 에러
-#  
 
-# 6번 ,14번
+
 #+===================================================================
 
 
@@ -274,3 +229,80 @@
 
 # 입력 예시: 0123456789 01234 01234567890 6789012345 012322456789
 # 출력 예시: True False False True False
+
+
+
+# s = "aaaabbbcczzzza"
+
+# #초기값을 설정합니다.
+# result = s[0] #반복문 실행되는 동안 문자열 형태로 반환되는 결과들을 담을 변수
+# count = 0 #반복되서 나오는 문자 수만큼 카운팅되는 값을 담을 변수
+
+# for i in s:
+#     if i == result[-1]: #result변수 마지막 문자와 비교합니다. else에서 result변수에 값이 추가되기 때문에 마지막 문자[-1]와 비교.
+#         count += 1
+#     else:
+#         result += str(count) + i #마지막 글자와 i가 다를 경우 카운팅된 값을 문자열 형태로 result 변수 마지막에 추가 해주고 i를 마지막 문자로 추가합니다.
+#         count = 1
+# result += str(count) #결과들이 담긴 변수에 마지막으로 카운팅된 값을 문자열 형태로 추가합니다.
+
+# print(result)
+
+
+# 입력 예시: aaabbcccccca
+# 출력 예시: a3b2c6a1
+
+# def str_remove(string): #[1,1,3]
+#     new_string = '' #새로 담을 문자열
+#     count = 1
+#     string = string + '\0'
+    
+#     for i in range(1,len(string)): #1,2, 
+#         print(f'{i}번째{string[i-1]} {string[i]}')
+#         if string[i-1] == string[i]:
+#             count += 1
+#         else:
+#             new_string = new_string + string[i-1] + str(count) + str_remove(string[i:])
+#             break
+#     return new_string
+
+# print(str_remove('aabbbcczzzza'))
+
+
+
+# def stringCompress_rec(string):
+#     ans = '' # 새로만들 변수
+#     count = 1
+#     string = string + '\0'
+#     print(string)
+#     for i in range(1, len(string)): # 1 ~ 10  
+#         if string[i - 1] == string[i]: #현재 값과 다음값이 같으면 
+#             count += 1
+#         else:
+#             ans += string[i - 1] + str(count) + stringCompress_rec(string[i:])
+#             break
+#     return ans
+
+# print(stringCompress_rec('bbbcczzzza'))
+
+
+# data = "aabbbcczzzza"
+# new_data = '' # 새로운 문자열 
+# count = 0
+# new_data += data[0]
+
+# for i in data:
+#     if i == new_data[-1]:
+#         count += 1
+#     else:
+#         new_data += str(count) + i
+#         count = 1
+# new_data += str(count)
+
+# print(new_data)
+
+# lis = [1,2,3]
+# st = '123'
+
+# # print(lis[-1])
+# print(lis[:-1])
